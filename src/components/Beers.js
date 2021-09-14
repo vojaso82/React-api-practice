7151f08892b6a5cdd69fd473ca71774720069fda
Vojislav Zaja <81269127+vojaso82@users.noreply.github.com>
2021-09-14 16:04:05 -0400

    started beer API

---
+++ src/components/Beers.js
@@ -0,0 +1,23 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'
+
+function Beers() {
+
+
+    const [search, setSearch] = useState('')
+    const [beer, setBeer] = useState([])
+
+
+
+
+
+
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Beers
